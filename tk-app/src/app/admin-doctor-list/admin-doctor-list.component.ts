c00807443107a45ffc83d33396b926e319722a3a
Thái Vĩnh Khang <92316262+KhangKT1@users.noreply.github.com>
2022-05-20 16:52:16 +0700

    edit new

--- tk-app/src/app/admin-doctor-list/admin-doctor-list.component.ts
+++ tk-app/src/app/admin-doctor-list/admin-doctor-list.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDoctorListComponent implements OnInit {
 
+  doctorlists: any;
+  errorMessage: string ='';
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
