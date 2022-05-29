cfaa686eb0df0bb4a715eee364356ceb63e24e8f
NguyenThu2512 <93482603+NguyenThu2512@users.noreply.github.com>
2022-05-29 23:54:26 +0700

    router all

--- tk-app/src/app/models/doctor-list.ts
+++ tk-app/src/app/models/doctor-list.ts
@@ -6,18 +6,3 @@ export interface DoctorList {
     "Position": string;
     "Description": string;
 }
-export class Doctor{
-    _id:any;
-   
-    DoctorName:string;
-    Department:string;
-    Description:string;
-    Position:string
-    constructor(){
-        this._id="";
-        this.DoctorName="";
-        this.Description="";
-        this.Department="";
-        this.Position= ""  ;
-    }
-}
