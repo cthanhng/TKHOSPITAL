3765bdb396d21831b7272ffdf95fcfdd386974cf
NguyenThu2512 <93482603+NguyenThu2512@users.noreply.github.com>
2022-05-30 03:12:46 +0700

    Revert "Revert "router all"""
    This reverts commit 657f1ab1c4092e8437b0e6369b4aba73d1c34060.

--- tk-app/src/app/models/doctor-list.ts
+++ tk-app/src/app/models/doctor-list.ts
@@ -6,3 +6,18 @@ export interface DoctorList {
     "Position": string;
     "Description": string;
 }
+export class Doctor{
+    _id:any;
+   
+    DoctorName:string;
+    Department:string;
+    Description:string;
+    Position:string
+    constructor(){
+        this._id="";
+        this.DoctorName="";
+        this.Description="";
+        this.Department="";
+        this.Position= ""  ;
+    }
+}
